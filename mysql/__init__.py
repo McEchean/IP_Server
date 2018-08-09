a81d9f7be2038f3fdc806757a9b51a0f79919e4f
McEchean <983821194w@sina.com>
2018-08-09 08:16:41 +0800

    ip

---
+++ mysql/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:98382
+# datetime:2018/8/8 21:53
+# software: PyCharm
---
+++ test_ip.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:98382
+# datetime:2018/8/9 0:15
+# software: PyCharm
